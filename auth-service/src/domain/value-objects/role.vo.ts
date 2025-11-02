d647f9da9b068c12b43bcf369535c425f74877bb
guymosso2022 <mossoguy2022@gmail.com>
2025-11-02 15:57:08 +0000

    fix(domain): handle InvalidIdRoleDomainException in role entity

--- auth-service/src/domain/value-objects/role.vo.ts
+++ auth-service/src/domain/value-objects/role.vo.ts
@@ -1,22 +1,44 @@
 import { InvalidRoleDomainException } from '../exceptions/invalid-role.domain-exception';
 import { RoleType } from '../type/role.type';
+import { PermissionVO } from './permission.vo';
+import { PermissionsVO } from './permissions.vo';
 
 export class RoleVO {
-  private constructor(private readonly value: RoleType) {}
+  // private constructor(private readonly value: RoleType) {}
 
   static create(role: RoleType): RoleVO {
     const validRoles: RoleType[] = ['ADMIN', 'USER', 'MERCHANT'];
     if (!validRoles.includes(role)) {
-      throw new InvalidRoleDomainException(role);
+      throw new InvalidRoleDomainException(`Invalid role: ${role}`);
     }
     return new RoleVO(role);
   }
 
-  getValue(): RoleType {
+  constructor(
+    private readonly value: string,
+    private permissions: PermissionsVO = PermissionsVO.create([]),
+  ) {
+    if (!value || value.trim() === '')
+      throw new InvalidRoleDomainException(value);
+  }
+
+  getValue(): string {
     return this.value;
   }
 
+  getPermissions(): PermissionsVO {
+    return this.permissions;
+  }
+
+  addPermission(permission: PermissionVO) {
+    this.permissions = this.permissions.addPermission(permission);
+  }
+
+  removePermission(permission: PermissionVO) {
+    this.permissions = this.permissions.removePermission(permission);
+  }
+
   equals(other: RoleVO): boolean {
-    return this.value === other.getValue();
+    return this.value === other.value;
   }
 }
