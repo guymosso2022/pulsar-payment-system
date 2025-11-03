44e6200fe4158bc74a545eb3c2e28a826061dc86
guymosso2022 <mossoguy2022@gmail.com>
2025-11-03 03:01:14 +0000

    feat(infrastructure): add prisma permission repository

--- auth-service/src/domain/ports/role.port.ts
+++ auth-service/src/domain/ports/role.port.ts
@@ -3,6 +3,6 @@ import { RoleIdVO } from '../value-objects/role-id.vo';
 
 export const IROLE_REPOSITORY_PORT = Symbol('IRoleRepositoryPort');
 export interface IPermissionRepositoryPort {
-  save(user: Role): Promise<Role>;
+  save(role: Role): Promise<Role>;
   findOneById(roleId: RoleIdVO): Promise<Role | null>;
 }
